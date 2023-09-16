e28feb0d1377015c64bcc597eb97836316a8d1b3
micah <micah@duckles.org>
2023-09-16 15:13:36 +1200

    added pieces and outputing them to the board

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ class Game:
         curses.start_color()
         curses.init_color(1,770,450,90)
         curses.init_color(2,530,230,30)
+        curses.init_color(3,0,0,0)
+        curses.init_color(4,1000,1000,1000)
         curses.init_color(3,1,1,1)
         curses.init_pair(1,1,2)
         curses.init_pair(2,2,1)
@@ -24,11 +26,19 @@ class Game:
         self.window.attrset(curses.color_pair(3))
 
     def gameloop(self):
+        self.pieces_to_board()
         self.board_out()
         while not self.gameover:
             event = self.window.getch()
             self.inputs(event)
         curses.endwin()
+
+    def pieces_to_board(self):
+        for piece in self.player1.pieces+self.player2.pieces:
+            self.board[piece.popsition[0]][piece.popsition[1]] = piece.piece_output
+        
+            
+            
 
     def inputs(self,event):
         if event == ord("q"):
@@ -57,14 +67,28 @@ class Game:
 class Piece:
     def __init__(self,piece,color:str,x:int,y:int):
         self.piece_type = piece 
+        self.piece_output = ["  ",color]   
+        if  self.piece_type.value==1: 
+            self.piece_output = ["♟♟",color]
+        if self.piece_type.value ==2: 
+            self.piece_output = ["♞♞",color]
+        if self.piece_type.value==3:
+            self.piece_output = ["♝♝",color]
+        if self.piece_type.value==4:
+            self.piece_output = ["♜♜",color]
+        if self.piece_type.value==5:
+            self.piece_output = ["♛♛",color]
+        if self.piece_type.value==6:
+            self.piece_output = ["♔♔",color]
         self.color = color
         self.popsition = [x,y]
                 
 
 class Player:
-    def __init__(self,turn:bool):
+    def __init__(self,turn:bool,pieces):
         self.gamestate = Gamestate.InProgress
         self.is_turn=turn
+        self.pieces = pieces
 
 class PieceType(Enum):
     Pawn=1 
@@ -82,8 +106,49 @@ class Gamestate(Enum):
 
 
 def main():
-    player1 = Player(True)
-    player2 = Player(False)
+    with open("output.txt","w") as f:
+        f.write("")
+    f.close()
+    player1_peices = [
+                      Piece(PieceType.Rook,"white",0,7),
+                      Piece(PieceType.Knight,"white",1,7),
+                      Piece(PieceType.Bishop,"white",2,7),
+                      Piece(PieceType.King,"white",3,7),
+                      Piece(PieceType.Queen,"white",4,7),
+                      Piece(PieceType.Bishop,"white",5,7),
+                      Piece(PieceType.Knight,"white",6,7),
+                      Piece(PieceType.Rook,"white",7,7),
+                      Piece(PieceType.Pawn,"white",0,6),
+                      Piece(PieceType.Pawn,"white",1,6),
+                      Piece(PieceType.Pawn,"white",2,6),
+                      Piece(PieceType.Pawn,"white",3,6), 
+                      Piece(PieceType.Pawn,"white",4,6), 
+                      Piece(PieceType.Pawn,"white",5,6), 
+                      Piece(PieceType.Pawn,"white",6,6), 
+                      Piece(PieceType.Pawn,"white",7,6), 
+                          ]
+
+    player2_peices = [
+                      Piece(PieceType.Rook,"black",0,0),
+                      Piece(PieceType.Knight,"black",1,0),
+                      Piece(PieceType.Bishop,"black",2,0),
+                      Piece(PieceType.Queen,"black",3,0),
+                      Piece(PieceType.King,"black",4,0),
+                      Piece(PieceType.Bishop,"black",5,0),
+                      Piece(PieceType.Knight,"black",6,0),
+                      Piece(PieceType.Rook,"black",7,0),
+                      Piece(PieceType.Pawn,"black",0,1),
+                      Piece(PieceType.Pawn,"black",1,1),
+                      Piece(PieceType.Pawn,"black",2,1),
+                      Piece(PieceType.Pawn,"black",3,1), 
+                      Piece(PieceType.Pawn,"black",4,1), 
+                      Piece(PieceType.Pawn,"black",5,1), 
+                      Piece(PieceType.Pawn,"black",6,1), 
+                      Piece(PieceType.Pawn,"black",7,1), 
+            ]
+
+    player1 = Player(True,player1_peices)
+    player2 = Player(False,player2_peices)
     game = Game(player1,player2)
     game.gameloop()
     
